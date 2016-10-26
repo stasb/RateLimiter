a8a7eb3f625a6fdd269f65f5dfc1957533d82045
Stas Belkov <stanislav.belkov@terem.com.au>
2016-10-27 01:16:26 +1100

    Complete throttle checking module, and calling of module within application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
   include RateThrottler
+
+  private
+
+  def throttle_requests!
+    throttle_result = perform_throttle_check
+
+    if throttle_result[:throttle]
+      render status: 429, json: { message: "Rate limit exceeded. Try again in #{throttle_result[:seconds_until_limit_lifted]} seconds" }
+    end
+  end
 end
--- lib/networking/rate_throttler.rb
+++ lib/networking/rate_throttler.rb
@@ -1,12 +1,48 @@
 module RateThrottler
-  class Limiter
-    def perform_throttling
+  class ThrottleChecker
+    TIME_INTERVAL_LIMIT_SECONDS = 3600
+    REQUEST_LIMIT = 100
+
+    def initialize(ip_address)
+      @ip_address = ip_address
+      @throttle_log = ThrottleLog.find_by(ip_address: ip_address)
+
+      @result = { throttle: false }
+    end
+
+    def check_for_throttling
+      if @throttle_log.present?
+        check_and_update_throttle_log
+      else
+        create_throttle_log
+      end
+
+      @result
+    end
+
+    private
+
+    def check_and_update_throttle_log
+      if Time.now > @throttle_log.expiry_time
+        @throttle_log.update_attributes(expiry_time: Time.now + TIME_INTERVAL_LIMIT_SECONDS, count: 1)
+      elsif @throttle_log.count >= REQUEST_LIMIT
+        @result[:throttle] = true
+        @result[:seconds_until_limit_lifted] = (@throttle_log.expiry_time - Time.now).round
+      else
+        current_count = @throttle_log.count
+
+        @throttle_log.update_attributes(count: current_count + 1)
+      end
+    end
+
+    def create_throttle_log
+      ThrottleLog.create(ip_address: @ip_address, expiry_time: Time.now + TIME_INTERVAL_LIMIT_SECONDS, count: 1)
     end
   end
 
-  def throttle_requests!
-    limiter = Limiter.new
+  def perform_throttle_check
+    throttle_checker = ThrottleChecker.new(request.remote_ip.to_s)
 
-    limiter.perform_throttling
+    throttle_checker.check_for_throttling
   end
 end
